be91c6a9a0119c16c9a7a9ea9689d0f06d7b9fe2
gnzlplcs <palaciosgm@outlook.com>
2022-08-31 16:18:10 -0500

    :art: style Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,13 @@
 const Footer = () => {
+  const today = new Date();
+
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h5>Team Member Allocation App - {today.getFullYear()}</h5>
+        </div>
+      </div>
     </footer>
   );
 };
