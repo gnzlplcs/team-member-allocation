76b7877c102e6ce4edcbc5aa4202f168ab058ae4
gnzlplcs <palaciosgm@outlook.com>
2022-08-31 16:09:48 -0500

    :building_construction: refactor code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,128 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Employees from "./Employees";
 import Footer from "./Footer";
 
 function App() {
+  const [selectedTeam, setSelectedTeam] = useState("TeamB");
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      fullName: "Bob Jones",
+      designation: "JavaScript Developer",
+      gender: "male",
+      teamName: "TeamA",
+    },
+    {
+      id: 2,
+      fullName: "Jill Bailey",
+      designation: "Node Developer",
+      gender: "female",
+      teamName: "TeamA",
+    },
+    {
+      id: 3,
+      fullName: "Gail Shepherd",
+      designation: "Java Developer",
+      gender: "female",
+      teamName: "TeamA",
+    },
+    {
+      id: 4,
+      fullName: "Sam Reynolds",
+      designation: "React Developer",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 5,
+      fullName: "David Henry",
+      designation: "DotNet Developer",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 6,
+      fullName: "Sarah Blake",
+      designation: "SQL Server DBA",
+      gender: "female",
+      teamName: "TeamB",
+    },
+    {
+      id: 7,
+      fullName: "James Bennet",
+      designation: "Angular Developer",
+      gender: "male",
+      teamName: "TeamC",
+    },
+    {
+      id: 8,
+      fullName: "Jessica Faye",
+      designation: "API Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 9,
+      fullName: "Lita Stone",
+      designation: "C++ Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 10,
+      fullName: "Daniel Young",
+      designation: "Python Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 11,
+      fullName: "Adrian Jacobs",
+      designation: "Vue Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 12,
+      fullName: "Devin Monroe",
+      designation: "Graphic Designer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+  ]);
+
+  const handleTeamSelectionChange = (event) => {
+    setSelectedTeam(event.target.value);
+  };
+
+  const handleEmployeeCardClick = (event) => {
+    const transformedEmployees = employees.map((employee) =>
+      employee.id === parseInt(event.currentTarget.id)
+        ? employee.teamName === selectedTeam
+          ? { ...employee, teamName: "" }
+          : { ...employee, teamName: selectedTeam }
+        : employee
+    );
+    setEmployees(transformedEmployees);
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Employees />
+      <Header
+        selectedTeam={selectedTeam}
+        teamMemberCount={
+          employees.filter((employee) => employee.teamName === selectedTeam)
+            .length
+        }
+      />
+      <Employees
+        employees={employees}
+        selectedTeam={selectedTeam}
+        handleEmployeeCardClick={handleEmployeeCardClick}
+        handleTeamSelectionChange={handleTeamSelectionChange}
+      />
       <Footer />
     </div>
   );
--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,111 +1,12 @@
-import { useState } from "react";
 import femaleProfile from "./images/femaleProfile.jpg";
 import maleProfile from "./images/maleProfile.jpg";
 
-const Employees = () => {
-  const [selectedTeam, setSelectedTeam] = useState("TeamB");
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      fullName: "Bob Jones",
-      designation: "JavaScript Developer",
-      gender: "male",
-      teamName: "TeamA",
-    },
-    {
-      id: 2,
-      fullName: "Jill Bailey",
-      designation: "Node Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 3,
-      fullName: "Gail Shepherd",
-      designation: "Java Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 4,
-      fullName: "Sam Reynolds",
-      designation: "React Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 5,
-      fullName: "David Henry",
-      designation: "DotNet Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 6,
-      fullName: "Sarah Blake",
-      designation: "SQL Server DBA",
-      gender: "female",
-      teamName: "TeamB",
-    },
-    {
-      id: 7,
-      fullName: "James Bennet",
-      designation: "Angular Developer",
-      gender: "male",
-      teamName: "TeamC",
-    },
-    {
-      id: 8,
-      fullName: "Jessica Faye",
-      designation: "API Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 9,
-      fullName: "Lita Stone",
-      designation: "C++ Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 10,
-      fullName: "Daniel Young",
-      designation: "Python Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 11,
-      fullName: "Adrian Jacobs",
-      designation: "Vue Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 12,
-      fullName: "Devin Monroe",
-      designation: "Graphic Designer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-  ]);
-
-  const handleTeamSelectionChange = (event) => {
-    setSelectedTeam(event.target.value);
-  };
-
-  const handleEmployeeCardClick = (event) => {
-    const transformedEmployess = employees.map((employee) =>
-      employee.id === parseInt(event.currentTarget.id)
-        ? employee.teamName === selectedTeam
-          ? { ...employee, teamName: "" }
-          : { ...employee, teamName: selectedTeam }
-        : employee
-    );
-    setEmployees(transformedEmployess);
-  };
-
+const Employees = ({
+  employees,
+  selectedTeam,
+  handleEmployeeCardClick,
+  handleTeamSelectionChange,
+}) => {
   return (
     <main className="container">
       <div className="row justify-content-center mt-3 mb-3">
